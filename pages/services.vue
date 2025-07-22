0017cf3f1de7f785999b6581a9637ea48aa77404
Altamash Rashid <asr.designmaster@gmail.com>
2025-07-23 04:14:36 +0530

    services page done

--- pages/services.vue
+++ pages/services.vue
@@ -8,31 +8,61 @@
                         <Icon name="famicons:airplane-sharp" class="text-2xl  text-primary" />
                     </span>
                 </h2>
-                <p class="text-center px-0 lg:px-60">With its specialized global services and advanced communications
-                    technology network and tracking systems, ZAK LOGISTICS. offers its customers a broad range of Total
+                <p class="text-center">With its specialized global services and advanced communications
+                    technology network and tracking systems, <br />ZAK LOGISTICS. offers its customers a broad range of
+                    Total
                     Logistics Solutions:</p>
             </div>
-
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3  [&>img:not(:first-child)]:mt-8 mt-8">
+                <div class="ocean_freight | relative">
+                    <div class="freight_curtain"></div>
+                    <h3 class="text-center text-white text-2xl">Ocean Freight Forwarding</h3>
+                </div>
+                <div class="ocean_freight | relative">
+                    <div class="freight_curtain"></div>
+                    <h3 class="text-center text-white text-2xl">Road Freight Forwarding</h3>
+                </div>
+                <div class="ocean_freight | relative">
+                    <div class="freight_curtain"></div>
+                    <h3 class="text-center text-white text-2xl">Air Freight Forwarding</h3>
+                </div>
+                <div class="ocean_freight | relative">
+                    <div class="freight_curtain"></div>
+                    <h3 class="text-center text-white text-2xl">Road Freight Forwarding</h3>
+                </div>
+                <div class="ocean_freight | relative">
+                    <div class="freight_curtain"></div>
+                    <h3 class="text-center text-white text-2xl">Air Freight Forwarding</h3>
+                </div>
+                <div class="ocean_freight | relative">
+                    <div class="freight_curtain"></div>
+                    <h3 class="text-center text-white text-2xl">Ocean Freight Forwarding</h3>
+                </div>
+            </div>
         </div>
     </section>
 
-    <section class="mission_vision |  bg-gray-900 px-4 lg:px-0">
+    <section class="mission_vision |  bg-gray-900">
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 ">
             <div class="">
-                <NuxtImg src="/images/international.webp" alt="international" class="w-full h-full object-cover" />
+                <NuxtImg src="/images/international.webp" alt="international" densities="x1" class="w-full h-60 lg:h-full object-cover" />
             </div>
-            <div class="py-16 pl-0 lg:pl-3">
-                <h2 class="flex items-center gap-3 text-white text-center lg:text-left">International Air & Sea Service
-                    Provider </h2>
-                <p
-                    class="text-xl text-white font-[weight:var(--font-weight-light)] w-full lg:max-w-2xl  text-center lg:text-left">
-                    ZAK Logistics offers a wide range of services with the scheduling and budget options that meet your
-                    requirements. Customers can select suitable schedules,
-                    costs and routes to any number of key destinations around the world. Planways support every
-                    logistical need of exporters and importers on a global basis, from packing, pick-up, and
-                    documentation, to customs clearance and delivery to the final destination.</p>
-                <p>Planways gives the complete solution to the customer for their required needs with the suitable
-                    options.</p>
+            <div class="py-16 px-4 lg:px-0">
+                <div class="pl-0 lg:pl-3 ">
+                    <h2 class="flex items-center gap-3 text-white text-center lg:text-left">International Air & Sea
+                        Service
+                        Provider </h2>
+                    <p
+                        class="text-xl text-white font-[weight:var(--font-weight-light)] w-full lg:max-w-2xl  text-center lg:text-left">
+                        ZAK Logistics offers a wide range of services with the scheduling and budget options that meet
+                        your
+                        requirements. Customers can select suitable schedules,
+                        costs and routes to any number of key destinations around the world. Planways support every
+                        logistical need of exporters and importers on a global basis, from packing, pick-up, and
+                        documentation, to customs clearance and delivery to the final destination.</p>
+                    <p>Planways gives the complete solution to the customer for their required needs with the suitable
+                        options.</p>
+                </div>
             </div>
         </div>
     </section>
@@ -70,3 +100,30 @@ useHead({
     ]
 })
 </script>
+
+<style scoped>
+.ocean_freight {
+    background-image: url('/images/ocean_freight.webp');
+    background-size: cover;
+    background-position: center;
+    height: 280px;
+    border-radius: 12px;
+    overflow: hidden;
+
+    .freight_curtain {
+        mix-blend-mode: multiply;
+        opacity: 1;
+        background: linear-gradient(0deg, #154765 0%, rgba(216, 216, 216, 0) 100%);
+        height: 150px;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+    }
+
+    h3 {
+        position: absolute;
+        bottom: 16px;
+        width: 100%;
+    }
+}
+</style>
